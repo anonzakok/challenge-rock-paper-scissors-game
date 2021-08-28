886a7c9ef4df9807ca2efcd096071c45034688c8
non.anon <non.anon@goventuredevelopment.com>
2021-08-28 22:37:20 +0700

    add audio

--- js/index.js
+++ js/index.js
@@ -3,9 +3,12 @@ const handOptions = {
   paper: "/assets/Paper.png",
   scissors: "/assets/Scissors.png",
 };
-let SCORE = 0;
+let SCOREUSER = 0;
+let SCORECP = 0;
 
 const pickUserHand = (hand) => {
+  const audioClickElem = document.querySelector(".click");
+  audioClickElem.play();
   let hands = document.querySelector(".hands");
   hands.style.display = "none";
 
@@ -38,36 +41,52 @@ const referee = (userHand, cpHand) => {
   }
   if (userHand == "paper" && cpHand == "scissors") {
     setDecision("YOU LOSE!");
+    setScoreComputer(SCORECP + 1);
   }
   if (userHand == "paper" && cpHand == "rock") {
     setDecision("YOU WIN!");
-    setScore(SCORE + 1);
+    setScoreUser(SCOREUSER + 1);
   }
   if (userHand == "rock" && cpHand == "paper") {
     setDecision("YOU LOSE!");
+    setScoreComputer(SCORECP + 1);
   }
   if (userHand == "rock" && cpHand == "scissors") {
     setDecision("YOU WIN!");
-    setScore(SCORE + 1);
+    setScoreUser(SCOREUSER + 1);
   }
   if (userHand == "scissors" && cpHand == "rock") {
     setDecision("YOU LOSE!");
+    setScoreComputer(SCORECP + 1);
   }
   if (userHand == "scissors" && cpHand == "paper") {
     setDecision("YOU WIN!");
-    setScore(SCORE + 1);
+    setScoreUser(SCOREUSER + 1);
   }
 };
 
 const setDecision = (decision) => {
   document.querySelector(".decision h1").innerText = decision;
+  const audioWinElem = document.querySelector(".win");
+  const audioLoseElem = document.querySelector(".lose");
+  const audioMissElem = document.querySelector(".miss");
+  if (decision == "YOU WIN!") {
+    audioWinElem.play();
+  } else if (decision == "YOU LOSE!") {
+    audioLoseElem.play();
+  } else if (decision == "It's a tie!") {
+    audioMissElem.play();
+  }
 };
 
-const setScore = (score) => {
-  SCORE = score;
-  document.querySelector(".score h1").innerText = score;
+const setScoreUser = (score) => {
+  SCOREUSER = score;
+  document.querySelector(".score .user").innerText = score;
 };
-
+const setScoreComputer = (score) => {
+  SCORECP = score;
+  document.querySelector(".score .computer").innerText = score;
+};
 const playAgain = () => {
   let hands = document.querySelector(".hands");
   hands.style.display = "flex";
